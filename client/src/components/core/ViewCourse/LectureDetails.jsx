afbba185bae8a4a3ecac6e5860be64ed3f10cd55
Andrés Biso <biso.andres@gmail.com>
2025-05-27 01:26:48 -0300

    feat: add some fixes to LectureDetails

--- client/src/components/core/ViewCourse/LectureDetails.jsx
+++ client/src/components/core/ViewCourse/LectureDetails.jsx
@@ -19,19 +19,19 @@ const LectureDetails = () => {
   const dispatch = useDispatch();
 
   const { token } = useSelector((state) => state.auth);
-  const { courseSectionData, completedLectures } = useSelector(
-    (state) => state.viewCourse
-  );
-  const { courseViewSidebar } = useSelector((state) => state.sidebar); // Fixed `courseViewSidebar`
+  const { courseSectionData, courseEntireData, completedLectures } =
+    useSelector((state) => state.viewCourse);
+  const { courseViewSidebar } = useSelector((state) => state.sidebar);
 
   const [lectureData, setLectureData] = useState(null);
+  const [previewSource, setPreviewSource] = useState('');
   const [videoEnded, setVideoEnded] = useState(false);
   const [loading, setLoading] = useState(false);
   const [selectedOption, setSelectedOption] = useState(null);
   const [isCorrect, setIsCorrect] = useState(false);
 
   useEffect(() => {
-    if (!courseSectionData.length) return;
+    if (!courseSectionData || courseSectionData.length === 0) return;
 
     const filteredSection = courseSectionData.find(
       (section) => section._id === sectionId
@@ -41,6 +41,27 @@ const LectureDetails = () => {
     );
     setLectureData(filteredLecture);
   }, [courseSectionData, sectionId, subSectionId]);
+
+  useEffect(() => {
+    (async () => {
+      if (!courseSectionData || courseSectionData.length === 0) return;
+      if (!courseId && !sectionId && !subSectionId) {
+        navigate(`/dashboard/enrolled-courses`);
+      } else {
+        const filteredData = courseSectionData.filter(
+          (course) => course._id === sectionId
+        );
+
+        const filteredLectureData = filteredData?.[0]?.subSection.filter(
+          (data) => data._id === subSectionId
+        );
+
+        if (filteredLectureData) setLectureData(filteredLectureData[0]);
+        setPreviewSource(courseEntireData.thumbnail);
+        setVideoEnded(false);
+      }
+    })();
+  }, [courseSectionData, courseId, sectionId, subSectionId]);
 
   const handleLectureCompletion = async () => {
     setLoading(true);
@@ -60,22 +81,24 @@ const LectureDetails = () => {
   };
 
   const isFirstLecture = () => {
+    if (!courseSectionData || courseSectionData.length === 0) return;
     const sectionIndex = courseSectionData.findIndex(
-      (sec) => sec._id === sectionId
+      (data) => data._id === sectionId
     );
     const subsectionIndex = courseSectionData[
       sectionIndex
-    ]?.subSection.findIndex((sub) => sub._id === subSectionId);
+    ]?.subSection.findIndex((data) => data._id === subSectionId);
     return sectionIndex === 0 && subsectionIndex === 0;
   };
 
   const isLastLecture = () => {
+    if (!courseSectionData || courseSectionData.length === 0) return;
     const sectionIndex = courseSectionData.findIndex(
-      (sec) => sec._id === sectionId
+      (data) => data._id === sectionId
     );
     const subsectionIndex = courseSectionData[
       sectionIndex
-    ]?.subSection.findIndex((sub) => sub._id === subSectionId);
+    ]?.subSection.findIndex((data) => data._id === subSectionId);
     return (
       sectionIndex === courseSectionData.length - 1 &&
       subsectionIndex === courseSectionData[sectionIndex].subSection.length - 1
@@ -83,12 +106,13 @@ const LectureDetails = () => {
   };
 
   const goToNextLecture = () => {
+    if (!courseSectionData || courseSectionData.length === 0) return;
     const sectionIndex = courseSectionData.findIndex(
-      (sec) => sec._id === sectionId
+      (data) => data._id === sectionId
     );
     const subsectionIndex = courseSectionData[
       sectionIndex
-    ]?.subSection.findIndex((sub) => sub._id === subSectionId);
+    ]?.subSection.findIndex((data) => data._id === subSectionId);
     const nextSubsection =
       courseSectionData[sectionIndex]?.subSection[subsectionIndex + 1];
 
@@ -105,12 +129,13 @@ const LectureDetails = () => {
   };
 
   const goToPrevLecture = () => {
+    if (!courseSectionData || courseSectionData.length === 0) return;
     const sectionIndex = courseSectionData.findIndex(
-      (sec) => sec._id === sectionId
+      (data) => data._id === sectionId
     );
     const subsectionIndex = courseSectionData[
       sectionIndex
-    ]?.subSection.findIndex((sub) => sub._id === subSectionId);
+    ]?.subSection.findIndex((data) => data._id === subSectionId);
     const prevSubsection =
       courseSectionData[sectionIndex]?.subSection[subsectionIndex - 1];
 
@@ -140,31 +165,78 @@ const LectureDetails = () => {
 
       {/* Render Content Based on type */}
       {lectureData?.type === 'video' && (
-        <Player
-          ref={playerRef}
-          aspectRatio="16:9"
-          playsInline
-          autoPlay
-          onEnded={() => setVideoEnded(true)}
-          src={lectureData?.video}
-        >
-          <BigPlayButton position="center" />
-          {videoEnded && (
-            <div className="absolute inset-0 flex flex-col items-center justify-center gap-4 bg-black bg-opacity-70">
-              {!completedLectures.includes(subSectionId) && (
-                <IconBtn
-                  disabled={loading}
-                  onclick={handleLectureCompletion}
-                  text={loading ? 'Cargando...' : 'Marcar como completado'}
-                />
+        <>
+          {!lectureData ? (
+            <img
+              src={previewSource}
+              alt="Preview"
+              className="h-full w-full rounded-md object-cover"
+            />
+          ) : (
+            <Player
+              ref={playerRef}
+              aspectRatio="16:9"
+              playsInline
+              autoPlay
+              onEnded={() => setVideoEnded(true)}
+              src={lectureData?.video}
+            >
+              <BigPlayButton position="center" />
+              {/* Render When Video Ends */}
+              {videoEnded && (
+                <div
+                  style={{
+                    backgroundImage:
+                      'linear-gradient(to top, rgb(0, 0, 0), rgba(0,0,0,0.7), rgba(0,0,0,0.5), rgba(0,0,0,0.1)',
+                  }}
+                  className="full absolute inset-0 z-[100] grid h-full place-content-center font-inter"
+                >
+                  {!completedLectures.includes(subSectionId) && (
+                    <IconBtn
+                      disabled={loading}
+                      onClick={() => handleLectureCompletion()}
+                      text={!loading ? 'Marcar como completado' : 'Cargando...'}
+                      customClasses="text-xl max-w-max px-4 mx-auto"
+                    />
+                  )}
+                  <IconBtn
+                    disabled={loading}
+                    onClick={() => {
+                      if (playerRef?.current) {
+                        playerRef?.current?.seek(0);
+                        setVideoEnded(false);
+                      }
+                    }}
+                    text="Volver a ver"
+                    customClasses="text-xl max-w-max px-4 mx-auto mt-2"
+                  />
+                  <div className="mt-10 flex min-w-[250px] justify-center gap-x-4 text-xl">
+                    {!isFirstLecture() && (
+                      <button
+                        disabled={loading}
+                        onClick={goToPrevLecture}
+                        className="blackButton"
+                      >
+                        Anterior
+                      </button>
+                    )}
+                    {!isLastLecture() && (
+                      <button
+                        disabled={loading}
+                        onClick={goToNextLecture}
+                        className="blackButton"
+                      >
+                        Siguiente
+                      </button>
+                    )}
+                  </div>
+                </div>
               )}
-              <IconBtn
-                onclick={() => playerRef.current.seek(0)}
-                text="Volver a ver"
-              />
-            </div>
+            </Player>
           )}
-        </Player>
+          <h1 className="mt-4 text-3xl font-semibold">{lectureData?.title}</h1>
+          <p className="pt-2 pb-6">{lectureData?.description}</p>
+        </>
       )}
 
       {lectureData?.type === 'text' && (
