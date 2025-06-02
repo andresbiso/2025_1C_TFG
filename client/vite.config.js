f3301045a9cc115c4a8c726740fee3459248c626
Andrés Biso <biso.andres@gmail.com>
2025-06-02 19:55:07 -0300

    chore: fix issue with localhost and 127.0.0.1

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
-import process from "node:process";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import process from 'node:process';
 
 export default defineConfig(({ mode }) => {
   // Load environment variables
-  const env = loadEnv(mode, process.cwd(), "");
+  const env = loadEnv(mode, process.cwd(), '');
 
   return {
     plugins: [react()],
     server: {
-      host: "0.0.0.0", // Enables access via localhost and 127.0.0.1
+      host: env.VITE_HOST || '0.0.0.0', // 0.0.0.0: Enables access via localhost and 127.0.0.1
       port: Number(env.VITE_PORT) || 8083,
     },
   };
